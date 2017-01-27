254dcdc025558b494ed19cac5a93432aa5d865ba
Lynette Lau <lynette.lau@sickkids.ca>
2017-01-27 10:18:29 -0500

    Changed config file

--- call_pipeline.pl
+++ call_pipeline.pl
@@ -9,7 +9,7 @@ use Time::Piece;
 ###########       Global Parameters   ##########################
 our ($sampleID, $postprocID, $fastqDir, $genePanel, $pipeline, $runfolder, $startPoint, $normalPair) = ('','','','','','','NEW','');
 
-my $pipeline_config_file = "/hpf/largeprojects/pray/clinical/config_test/v5.1_pipeline_config_test.txt"; #Future will be passed from the thing1 cmd
+my $pipeline_config_file = "/hpf/largeprojects/pray/clinical/config_test/v5.1_pipeline_config.txt"; #Future will be passed from the thing1 cmd
 my $genepanel_config_file = "/hpf/largeprojects/pray/clinical/config/v5.1_gene_panels_config.txt"; #Future will be passed from the thing1 cmd
 
 GetOptions ("sampleID|s=s" => \$sampleID,
